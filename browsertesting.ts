388f5a5f17e58c97a767b6853b32c2dac93186f3
Brent Broeckx <brent.broeckx@cegeka.com>
2023-02-22 11:39:38 +0100

    Set google as default webpage

--- browsertesting.ts
+++ browsertesting.ts
@@ -3,6 +3,6 @@ const { chromium, firefox, webkit } = require('@playwright/test');
 (async () => {
   const browser = await webkit.launch();  // Or 'firefox' or 'webkit'.
   const page = await browser.newPage();
-  await page.goto('https://www.gonna.be/');
+  await page.goto('https://www.google.com/');
   // other actions...
 })();
